f73b97d05ac07e69d6eae654d1b15b3fa568f057
Mashqur <mashqur.himel@gmail.com>
2022-07-09 20:06:11 +0600

    Initial Commit

---
+++ NotificationCenterTest/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  NotificationCenterTest
+//
+//  Created by Mashqur Habib Himel on 7/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addObserver()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        throwNotification()
+    }
+    
+    func addObserver() {
+        let nc = NotificationCenter.default
+        nc.addObserver(self, selector: #selector(catchNotification), name: .notificationName , object: nil)
+    }
+    
+    func throwNotification() {
+        NotificationCenter.default.post(name: .notificationName, object: nil)
+    }
+    
+    @objc func catchNotification() {
+        print("Mahir triggered")
+    }
+}
+
+extension Notification.Name {
+    static let notificationName = Notification.Name("Mahir")
+}
+
